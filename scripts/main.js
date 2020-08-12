b0332dfc626c1c2b9524b907aef120cddddfb22b
Bekzhan111 <68604058+Bekzhan111@users.noreply.github.com>
2020-08-12 20:48:35 +0600

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -66,18 +66,3 @@ function countdown(dateEnd) {
   }
    
   countdown ('09/15/2020 00:00:00 AM');
-
-
-  var swiper = new Swiper(".swiper-container", {
-    slidesPerView: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  });
